890da01a8d2dff8f574505597a48aac3c4ca9bd1
Shadowmataj <christianmataj1@gmail.com>
2025-03-05 20:27:03 -0600

    add migrations to the project.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 from db import db
 from blocklist import BLOCKLIST
@@ -32,6 +33,11 @@ def create_app(db_url=None):
     )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
+
+    migrate = Migrate(app=app, db=db)
+
+    with app.app_context():
+        db.create_all()
 
     api = Api(app)
 
@@ -87,8 +93,6 @@ def create_app(db_url=None):
                     401,
         )
 
-    with app.app_context():
-        db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
--- schemas.py
+++ schemas.py
@@ -12,6 +12,7 @@ class PlainItemSchema(Schema):
 
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
+    description = fields.Str(required=True)
     price = fields.Float(required=True)
 
 
